7b547fc1c63a8cc35020235234ec1487e9435d21
Fambot_RaysonNB <97947041+RaysonNB@users.noreply.github.com>
2025-07-09 22:28:24 +0800

    Update task3_2.py

--- task3_2.py
+++ task3_2.py
@@ -43,48 +43,56 @@ while True:
         api_url = "http://192.168.50.147:8888/Fambot"
         response = requests.post(api_url, json=questions)
     if dictt["Steps"] == "feature":
-        path_sample = "C:/Users/rayso/Desktop/python/guest1.jpg"  # Use raw string to handle backslashes
-        # Prepare the prompt template
-        sample_txt = ""
-        img = PIL.Image.open(path_sample)
-        promt = '''
-        question1: how old is the guy, give me a range
-        question2: he is male or female?
-        question3: what color of colthes he is wearing
-        
-        answer the question in complete sentence
-        entire_answer = question1 answer + question2 answer + question3 answer
-        just need one sentence
-        answer format: ******[entire_answer]******)
-        '''
-        response = model.generate_content([img, promt])
-        # response = model.generate_content([img2,"where have empty seat , just give me number in [1,2,3,4,5], there should be 4 numbers, answer format: ******[numbers]******"])
-        import re
+        while True:
+            path_sample = "C:/Users/rayso/Desktop/python/guest1.jpg"  # Use raw string to handle backslashes
+            # Prepare the prompt template
+            sample_txt = ""
+            img = PIL.Image.open(path_sample)
+            promt = '''
+            question1: how old is the guy, give me a range
+            question2: he is male or female?
+            question3: what color of colthes he is wearing
+            question4: His hair color
+            
+            answer the question in complete sentence
+            entire_answer = question1 answer, question2 answer, question3 answer, question 4 answer
+            just need one sentence
+            answer format: ******[entire_answer]******)
+            
+            you must answer
+            '''
+            response = model.generate_content([img, promt])
+            # response = model.generate_content([img2,"where have empty seat , just give me number in [1,2,3,4,5], there should be 4 numbers, answer format: ******[numbers]******"])
+            import re
 
-        a = str(response)
-        print(a)
-        # Using a regular expression to find text between asterisks
-        matches = re.findall(r'\*\*\*\*\*\*(.*?)\*\*\*\*\*\*', a)
+            a = str(response)
+            print(a)
+            # Using a regular expression to find text between asterisks
+            matches = re.findall(r'\*\*\*\*\*\*(.*?)\*\*\*\*\*\*', a)
 
-        # Printing the extracted values
-        answergg=""
-        for match in matches:
-            g = match.strip()
-            answergg=g
-            print(g)  # Output: [1, 2, 4, 5]
-        questions = {
-            "Question1": "None",
-            "Question2": "None",
-            "Question3": "None",
-            "Steps": 100,
-            "Voice": answergg,
-            "Questionasking": "None",
-            "answer": "None"
-        }
-        api_url = "http://192.168.50.147:8888/Fambot"
-        response = requests.post(api_url, json=questions)
-        result = response.json()
-        print(result)
+            # Printing the extracted values
+            answergg=""
+            for match in matches:
+                g = match.strip()
+                answergg=g
+                print(g)  # Output: [1, 2, 4, 5]
+            if answergg != "":
+                questions = {
+                    "Question1": "None",
+                    "Question2": "None",
+                    "Question3": "None",
+                    "Steps": 100,
+                    "Voice": answergg,
+                    "Questionasking": "None",
+                    "answer": "None"
+                }
+                api_url = "http://192.168.50.147:8888/Fambot"
+                response = requests.post(api_url, json=questions)
+                result = response.json()
+                print(result)
+
+                break
+            time.sleep(2)
     if dictt["Steps"] == "task3":
         print("task3")
         promt = dictt["Questionasking"]
