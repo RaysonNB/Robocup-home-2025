2a83d00ac0580dae72f27d2474d6ee17e92eca02
IsaacMak <95964568+IsaacMak2009@users.noreply.github.com>
2025-07-16 06:40:05 +0800

    Create ri2.py

---
+++ ri2.py
@@ -0,0 +1,58 @@
+import rospy
+import cv2, time
+from loguru import logger
+from LemonEngine.sensors import Camera
+from LemonEngine.hardwares.respeaker import Respeaker
+from LemonEngine.hardwares.chassis import Chassis, Navigator
+
+
+def main():
+    chassis_move = Chassis()
+    navigator = Navigator()
+    
+    respeaker = Respeaker(enable_espeak_fix=True)
+    cam = Camera("/camera/depth/image_raw", "passthrough")
+    width, height = cam.width, cam.height
+    cx, cy = width // 2, height // 2
+    rate = rospy.Rate(20)
+
+    zero_count = 0
+    while not rospy.is_shutdown():
+        rate.sleep()
+        frame = cam.get_frame()
+        depth = frame[cy, cx]
+        logger.debug(f"Depth: {depth}, Captured Continue {zero_count} zero points")
+
+        zero_count += 1 if depth == 0 else (-zero_count)
+        if depth > 2000 or zero_count > 100:
+            respeaker.say("The door is opened")
+
+            time.sleep(5)
+            for i in range(80):
+                chassis_move.set_linear(0.25)
+                rate.sleep()
+            time.sleep(3)
+            navigator.clear_costmaps()
+            break
+
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+    navigator.move_to(*[4.130, 3.064, -3.074])
+    time.sleep(2)
+    respeaker.say("Testing testing")
+    prompt = ""
+    while prompt != "ok":
+        prompt = input(">>")
+
+    navigator.move_to(*[-1.032, 7.978, -3.092])
+    time.sleep(2)
+
+    respeaker.say("Done")
+
+if __name__ == '__main__':
+    rospy.init_node('test_camera', anonymous=True)
+    main()
