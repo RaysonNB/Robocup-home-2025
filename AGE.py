d97efe275f02bc4ec2f326c53ba30ce1442cbf98
Fambot_RaysonNB <97947041+RaysonNB@users.noreply.github.com>
2025-07-15 20:57:32 -0300

    Update AGE.py

--- AGE.py
+++ AGE.py
@@ -28,6 +28,7 @@ locations = {
     "sink": [0.396,6.064,-1.611],
     "waste basket": [0.47,5.11,-3.0],
     "refrigerator": [0.47,6.95,-3.0],
+        "office": [4.357,2.533,0.663],
 }
 # front 0 back 3.14 left 90 1.5 right 90 -1.5
 cout_location = {
