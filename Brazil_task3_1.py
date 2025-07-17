81cc790d151359bbc9a87340d4a12ce7e21f762f
Fambot_RaysonNB <97947041+RaysonNB@users.noreply.github.com>
2025-07-17 17:59:52 +0800

    Update Brazil_task3_1.py

--- Brazil_task3_1.py
+++ Brazil_task3_1.py
@@ -121,6 +121,7 @@ def speak11(g):
     print(g)
     time.sleep(0.3)
 
+
 def speak(g):
     print("[robot say]:", end=" ")
     os.system(f'espeak -s 180 "{g}"')
@@ -282,7 +283,7 @@ if __name__ == "__main__":
     confirm_command = 0
     for nigga_i in [1, 2]:
         check_cnt = 0
-        if nigga_i==2:
+        if nigga_i == 2:
             walk_to("guest")
         say_cnt = 0
         while not rospy.is_shutdown():
@@ -315,12 +316,12 @@ if __name__ == "__main__":
                     cx = (x2 - x1) // 2 + x1
                     cy = (y2 - y1) // 2 + y1
                     _, _, d = get_real_xyz(code_depth, cx, cy, 2)
-                    if nigga_i==1 and cx>=260 and cx<=380: #320
+                    if nigga_i == 1 and cx >= 260 and cx <= 380:  # 320
                         if score > 0.65 and class_id == 0 and d != 0 and d <= 3000:
                             check_cnt += 1
                             mx1, my1, mx2, my2 = x1, y1, x2, y2
                             yn = 1
-                    elif nigga_i==2:
+                    elif nigga_i == 2:
                         if score > 0.65 and class_id == 0 and d != 0 and d <= 1800:
                             check_cnt += 1
                             mx1, my1, mx2, my2 = x1, y1, x2, y2
@@ -344,7 +345,7 @@ if __name__ == "__main__":
                     cx = (x2 - x1) // 2 + x1
                     cy = (y2 - y1) // 2 + y1
                     _, _, d = get_real_xyz(code_depth, cx, cy, 2)
-                    if score > 0.65 and class_id == 0 and d != 0 and d <= 3000 and cx>=270 and cx<=370:
+                    if score > 0.65 and class_id == 0 and d != 0 and d <= 3000 and cx >= 270 and cx <= 370:
                         check_cnt += 1
                         mx1, my1, mx2, my2 = x1, y1, x2, y2
                         yn = 1
@@ -372,7 +373,8 @@ if __name__ == "__main__":
                 name_cnt = "none"
                 s = s.lower()
                 if say_cnt == 0:
-                    speak11("Dear guest, Please answer my following question in complete sentence with louder voice, for example my name is Fambot")
+                    speak11(
+                        "Dear guest, Please answer my following question in complete sentence with louder voice, for example my name is Fambot")
                     speak11("it is better to stand in front of me now, thank you")
                     time.sleep(1)
                     speak1("what is your name")
@@ -431,10 +433,10 @@ if __name__ == "__main__":
             if step == "interest":
                 interest_name = "none"
                 s = s.lower()
-                if s!="":
+                if s != "":
                     if "is" in s and "interest" in s:
                         interest_name = s.split("interest is")[-1]
-                        
+
                 if (interest_name == "none" and s != ""):
                     speak("please repeat")
                 if interest_name != "none":
@@ -601,10 +603,12 @@ if __name__ == "__main__":
                 if nigga_i == 2:
                     seat_turn(guest1_seat)
                     time.sleep(1)
-                    speak("dear guest " + pre_name + " this is the second guest " + name + " favourite drink is " + drink_name + " interest is " + interest_name)
+                    speak(
+                        "dear guest " + pre_name + " this is the second guest " + name + " favourite drink is " + drink_name + " interest is " + interest_name)
                     seat_turn_back(guest1_seat)
                     turn(-60)  # chassis left
-                    speak("dear guest " + name + " this is the first guest " + pre_name + " favourite drink is " + pre_drink + " interest is " + pre_interest)
+                    speak(
+                        "dear guest " + name + " this is the first guest " + pre_name + " favourite drink is " + pre_drink + " interest is " + pre_interest)
                     time.sleep(1)
                     turn(60)
                 step = "tell"
@@ -635,7 +639,7 @@ if __name__ == "__main__":
                             speech_robot_guest2 = aaa
                             break
                 if nigga_i == 2:
-                    question = "give me one similarity between the interest of " + interest_name + ", " + pre_interest + " and " + host_interest_name
+                    question = "give me one similarity between the interest of " + interest_name + ", " + pre_interest + " and " + host_interest_name+"    (just use 15 words to describe)"
                     gg = post_message_request("interest", question, question)
 
                     while True:
@@ -647,7 +651,8 @@ if __name__ == "__main__":
                             gg = post_message_request("-1", "", "")
                             aaa = dictt["Voice"].lower()
                             speech_robot_guest2 = aaa
-                            speak("here is a similarity between the interest of " + interest_name + " and " + pre_interest + "and" + host_interest_name)
+                            speak(
+                                "here is a similarity between the interest of " + interest_name + " and " + pre_interest + " and " + host_interest_name)
                             time.sleep(1)
                             speak(aaa)
                             break
